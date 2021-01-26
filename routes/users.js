43933dbdc0d6ba2ba19413f7bd4a8ebb498bcc59
Gamra01 <ilyassstudies2017@gmail.com>
2021-01-26 08:00:20 +0100

    access to all admins

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const { requireSignIn, isAuth, isAdmin } = require('../middlewares/auth')
 const router = express.Router();
 
 
-router.get('/profile/:userId', requireSignIn, isAuth, isAdmin, getOneUser)
+router.get('/profile/:userId', requireSignIn, isAuth, getOneUser)
 
 router.param('userId', userById)
 
