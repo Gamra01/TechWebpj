bf6c200b8d6d64ed78433b485d6d0f6026c3b122
Gamra01 <ilyassstudies2017@gmail.com>
2021-01-27 09:35:33 +0100

    Product Validation using joi

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 const fs = require('fs');
-
+const Joi = require('joi');
 const formidable = require('formidable');
 
 exports.createProduct = (req, res) => {
@@ -18,9 +18,30 @@ exports.createProduct = (req, res) => {
         let product = new Product(fields);
 
         if(files.photo){
+            if(files.photo.size > Math.pow(10, 6)){
+                return res.status(400).json({
+                    error: 'Image size should be less than 1mb'
+                })
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
 
+        }
+
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            description: Joi.string().required(),
+            price: Joi.required(),
+            quantity: Joi.required(),
+            category: Joi.required()
+        })
+        
+        const { error } = schema.validate(fields);
+
+        if(error) {
+            return res.status(400).json({
+                error: error.details[0].message
+            })
         }
         product.save((err, product) => {
             if(err){
