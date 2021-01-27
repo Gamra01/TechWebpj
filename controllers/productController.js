22df81344d274f2f86b993b4f0e9a55c711df488
Gamra01 <ilyassstudies2017@gmail.com>
2021-01-27 10:46:54 +0100

    update product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,10 @@
 const Product = require('../models/product');
+const _ = require('lodash');
 const fs = require('fs');
 const Joi = require('joi');
+
 const formidable = require('formidable');
-const { nextTick } = require('process');
+
 
 exports.createProduct = (req, res) => {
 
@@ -57,6 +59,61 @@ exports.createProduct = (req, res) => {
     })
 }
 
+
+exports.updateProduct = (req, res) => {
+
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, files) =>{
+        
+        if(err){
+            return res.status(400).json({
+                error: 'Image could not upload !'
+            })
+        }
+        let product = req.product;
+
+        product = _.extend(product, fields)
+
+        if(files.photo){
+            if(files.photo.size > Math.pow(10, 6)){
+                return res.status(400).json({
+                    error: 'Image size should be less than 1mb'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+
+        }
+
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            description: Joi.string().required(),
+            price: Joi.required(),
+            quantity: Joi.required(),
+            category: Joi.required()
+        })
+        
+        const { error } = schema.validate(fields);
+
+        if(error) {
+            return res.status(400).json({
+                error: error.details[0].message
+            })
+        }
+        product.save((err, product) => {
+            if(err){
+                return res.status(400).json({
+                    err: 'Product not updated'
+                })
+            }
+            res.json({
+                product
+            })
+        })
+    })
+}
 
 exports.productById = (req, res, next, id) => {
     
